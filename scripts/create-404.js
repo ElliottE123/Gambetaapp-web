e582878e9eba5983ca290e692df847757ab62729
ElliottE123 <eeager@terpmail.umd.edu>
2026-01-21 22:05:22 -0500

    Simplify 404.html to be identical to index.html for React Router

--- scripts/create-404.js
+++ scripts/create-404.js
@@ -9,29 +9,9 @@ const __dirname = path.dirname(__filename);
 const indexPath = path.join(__dirname, '../dist/index.html');
 const indexContent = fs.readFileSync(indexPath, 'utf8');
 
-// Create 404.html with redirect script
-const redirectScript = `
-    <script>
-      // Single Page Apps for GitHub Pages - Redirect to index.html with path in query string
-      var pathSegmentsToKeep = 0;
-      var l = window.location;
-      if (l.pathname !== '/' && !l.pathname.startsWith('/assets/') && !l.pathname.startsWith('/images/')) {
-        l.replace(
-          l.protocol + '//' + l.hostname + (l.port ? ':' + l.port : '') +
-          '/?/' +
-          l.pathname.slice(1).split('/').join('/').replace(/&/g, '~and~') +
-          (l.search ? '&' + l.search.slice(1).replace(/&/g, '~and~') : '') +
-          l.hash
-        );
-      }
-    </script>`;
-
-// Insert the redirect script right after the viewport meta tag
-const viewportRegex = /(<meta name="viewport"[^>]*>)/;
-const updatedContent = indexContent.replace(viewportRegex, `$1${redirectScript}`);
-
-// Write 404.html
+// Simply copy index.html to 404.html
+// GitHub Pages will serve 404.html for any 404, and React Router will handle routing
 const outputPath = path.join(__dirname, '../dist/404.html');
-fs.writeFileSync(outputPath, updatedContent);
+fs.writeFileSync(outputPath, indexContent);
 
-console.log('404.html created with redirect script');
+console.log('404.html created (identical to index.html)');
